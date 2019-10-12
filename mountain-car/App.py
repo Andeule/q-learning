4b5ee4cfee0ef7da576aee7aad51a0a7a93db75a
Andre Hartmann <andrehartmann1993@hotmail.de>
2019-10-12 12:48:37 +0200

    add epsilon to introduce randomness in our system

--- mountain-car/App.py
+++ mountain-car/App.py
@@ -7,14 +7,19 @@ env = gym.make("MountainCar-v0")
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 EPISODES = 25000
-
 #show us we you are right now after every 2000 episodes
-SHOW_EVERY = 200
+SHOW_EVERY_EPISODE = 200
 
 # With this we cut our oberservation space into 20 junks
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 # Find out the size of each junk [0.09  0.007]
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+
+epsilon = 0.5
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES // 2
+
+epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 # q table shape (20, 20, 3)
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
@@ -32,7 +37,7 @@ print(q_table[discrete_state])
 print(discrete_state)
 
 for episode in range(EPISODES):
-    if episode % SHOW_EVERY == 0:
+    if episode % SHOW_EVERY_EPISODE == 0:
         print("Episode: "+str(episode))
         render = True
     else:
@@ -41,7 +46,11 @@ for episode in range(EPISODES):
     discrete_state = get_descrete_state(env.reset())
     done = False
     while not done:
-        action = np.argmax(q_table[discrete_state])
+
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
         new_state, reward, done, _ = env.step(action)
         new_discrete_state = get_descrete_state(new_state)
         if render:
@@ -55,10 +64,11 @@ for episode in range(EPISODES):
             q_table[discrete_state + (action,)] = new_q
         elif new_state[0] >= env.goal_position:
             print(f"We made it on {episode}")
-            env.render()
             q_table[discrete_state + (action,)] = 0
 
         discrete_state = new_discrete_state
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        epsilon -= epsilon_decay_value
 
 env.close()
 
