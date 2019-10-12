a1b20b1ce89933947c2da2944eed7d875d159af3
Andre Hartmann <andrehartmann1993@hotmail.de>
2019-10-12 11:59:34 +0200

    initialize q_table

--- mountain-car/App.py
+++ mountain-car/App.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 env = gym.make("MountainCar-v0")
 env.reset()
@@ -14,12 +15,17 @@ discrete_os_win_size = (env.observation_space.high-env.observation_space.low) / 
 print("Discrete os size:"+str(DISCRETE_OS_SIZE))
 print("Discrete os win size:" + str(discrete_os_win_size))
 
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+print(q_table.shape)
+
+
 
 done = False
 
 while not done:
     action = 2
     new_state, reward, done, _ = env.step(action)
+    print(str(reward) + str(new_state))
     env.render()
 
 env.close()
